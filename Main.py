56aeb7c9d52e62ee24bc2dc772b5370dc2ed9295
Jizhen Wang <jzwang43@gmail.com>
2018-08-04 00:57:06 -0400

    Scrape NBA roster from ESPN

--- Main.py
+++ Main.py
@@ -7,23 +7,16 @@ html = urlopen(req).read().decode('utf-8')
 soup = BeautifulSoup(html, "html.parser")
 
 team_links = soup.find_all('a', {'href': re.compile('http://www.espn.com/nba/team/_/name/')})
-
+f = open("roster.txt", 'w')
 for link in team_links:
     link = link['href']
     link = link[0:29] + 'roster/_' + link[30:]
-    print(link.rsplit('/',1)[1].replace('-',' ').title())
+    f.write('#'+link.rsplit('/',1)[1].replace('-',' ').title()+'\n')
     req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
     html = urlopen(req).read().decode('utf-8')
     soup = BeautifulSoup(html, "html.parser")
-
-    #player_links = soup.find_all('a', {'href': re.compile('http://www.espn.com/nba/player/_/id/')})
-
-    #for link2 in player_links:
-        #f.write(link2.text + '\n')
     count = 0
-    f = open(link.rsplit('/', 1)[1] + ".txt", 'w')
     for item in soup.find("table", {"class": "tablehead"}).children:
         count += 1
         if count > 2:
-            prev = '0'
-            print(item.get_text(" "))
+            f.write(item.get_text(" ").replace(u'\xa0', u' ')+'\n')
